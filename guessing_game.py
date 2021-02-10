4563d97dfb2bb4ae2422376e2ea097cef84619af
nosnowowie93347 <nosnowowie@Gmail.com>
2021-02-10 13:25:25 -0800

    test

---
+++ guessing_game.py
@@ -0,0 +1,42 @@
+import random
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+
+
+n = random.randint(1, 99)
+
+guess = int(input("Enter an integer from 1 to 99: "))
+
+while n != "guess":
+
+  print
+
+  if guess < n:
+
+    print("guess is low")
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(18,GPIO.LOW)
+    guess = int(input("Enter an integer from 1 to 99: "))
+
+  elif guess > n:
+
+    print ("guess is high")
+    GPIO.output(27,GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(27,GPIO.LOW)
+    guess = int(input("Enter an integer from 1 to 99: "))
+
+  else:
+
+    print ("you guessed it!")
+    GPIO.output(17,GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(17,GPIO.LOW)
+
+    break
